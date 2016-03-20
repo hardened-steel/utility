bbd16e565407b9c966838b287e0f4c9135197d84
Kirill Pinegin <keldgaden@gmail.com>
2016-03-20 19:31:09 +0300

    add std lib include directive

--- instance.hpp
+++ instance.hpp
@@ -7,6 +7,8 @@
 #ifndef INSTANCE_HPP_
 #define INSTANCE_HPP_
 
+#include <utility>
+#include <cstdint>
 #include <type_traits>
 
 #define def_friend_boperator(Operator) template<class A, class B> friend auto operator Operator(A&& a, B&& b) -> decltype(a.get() Operator b.get()) { return a.get() Operator b.get(); }
@@ -16,13 +18,13 @@ namespace utility {
 	namespace details {
 		class instance_control_block
 		{
-			size_t counter = 1;
+			std::size_t counter = 1;
 		public:
 			virtual instance_control_block* clone() const = 0;
-			virtual uintptr_t offset(uintptr_t ptr) const = 0;
-			virtual uintptr_t getptr() const = 0;
-			size_t inc() { return counter += 1; }
-			size_t dec() { return counter -= 1; }
+			virtual std::uintptr_t offset(std::uintptr_t ptr) const = 0;
+			virtual std::uintptr_t getptr() const = 0;
+			std::size_t inc() { return counter += 1; }
+			std::size_t dec() { return counter -= 1; }
 			bool unique() const { return counter == 1; }
 			virtual ~instance_control_block() {}
 		};
@@ -31,8 +33,8 @@ namespace utility {
 			T object;
 		public:
 			template<class...TArgs> unique_control_block(TArgs&&...args): object(std::forward<TArgs>(args)...) {}
-			uintptr_t offset(uintptr_t ptr) const override { return ptr - getptr(); }
-			uintptr_t getptr() const override { return (uintptr_t)&object; }
+			std::uintptr_t offset(std::uintptr_t ptr) const override { return ptr - getptr(); }
+			std::uintptr_t getptr() const override { return (std::uintptr_t)&object; }
 			instance_control_block* clone() const override { return nullptr; }
 			~unique_control_block() {}
 		};
@@ -41,12 +43,14 @@ namespace utility {
 			T object;
 		public:
 			template<class...TArgs> shared_control_block(TArgs&&...args): object(std::forward<TArgs>(args)...) {}
-			uintptr_t offset(uintptr_t ptr) const override { return ptr - getptr(); }
-			uintptr_t getptr() const override { return (uintptr_t)&object; }
+			std::uintptr_t offset(std::uintptr_t ptr) const override { return ptr - getptr(); }
+			std::uintptr_t getptr() const override { return (std::uintptr_t)&object; }
 			instance_control_block* clone() const override { return new shared_control_block(object); }
 			~shared_control_block() {}
 		};
 	}
+
+	constexpr bool copyable = true;
 
 	template<class T, bool Copyable = std::is_copy_constructible<T>::value> class instance;
 
@@ -131,7 +135,7 @@ namespace utility {
 		T& get() {
 			if(!cblock->unique()) {
 				auto new_cblock = cblock->clone();
-				auto offset = cblock->offset((uintptr_t)object);
+				auto offset = cblock->offset((std::uintptr_t)object);
 				reset();
 				cblock = new_cblock;
 				object = (T*)(cblock->getptr() + offset);
@@ -269,7 +273,7 @@ namespace utility {
 		T& get() {
 			if(!cblock->unique()) {
 				auto new_cblock = cblock->clone();
-				auto offset = cblock->offset((uintptr_t)object);
+				auto offset = cblock->offset((std::uintptr_t)object);
 				reset();
 				cblock = new_cblock;
 				object = (T*)(cblock->getptr() + offset);
